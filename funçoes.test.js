1a351d51f2b3850e7ae6b900051e23b47c124904
Vinicius Luis Nascimento <vinicius.kup@gmail.com>
2022-11-16 20:15:21 -0300

    adicionado o jest

--- funçoes.test.js
+++ funçoes.test.js
@@ -20,14 +20,14 @@ describe('validarCampos testes', () => {
       expect(validarCampos(nome, email, mensagem)).toBe('E-mail inválido')
     })
     test('Validar Mensagem menos que 10 caracteres', () => {
-      const nome = 'rodrigo'
+      const nome = 'nome'
       const email = 'teste@gmail.br'
       const mensagem = 'msg'
       expect(validarCampos(nome, email, mensagem)).toBe('Mensagem com no mínimo 10 caracteres')
     })
     test('Validar todos os campos', () => {
-      const nome = 'rodrigo'
-      const email = 'rodrigo@einstein.br'
+      const nome = 'nome'
+      const email = 'teste@gmail.br'
       const mensagem = 'mensagem grande'
       expect(validarCampos(nome, email, mensagem)).toBe(false)
     })
